bcf4f04c2f11acd5d890081e90bf7c270a16ae7d
Paul Guillen <paulguillen190@gmail.com>
2025-02-16 14:59:16 -0600

    Login Screen started basic texts

--- app/src/main/java/com/devpaul/indriver/presentation/screens/auth/login/LoginScreen.kt
+++ app/src/main/java/com/devpaul/indriver/presentation/screens/auth/login/LoginScreen.kt
@@ -1,17 +1,24 @@
 package com.devpaul.indriver.presentation.screens.auth.login
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -27,22 +34,27 @@ fun LoginScreen() {
             )
             .padding(paddingValues)
         ) {
-            Column {
+            Column(
+                modifier = Modifier.fillMaxHeight(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.Start,
+            ) {
                 Text(
-                    text = "Inicio de sesión",
+                    text = "Login",
                     color = Color.White,
-                    fontSize = 24.sp,
+                    fontSize = 30.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = Modifier
+                    modifier = Modifier.rotate(90f)
                 )
+                Spacer(modifier = Modifier.height(140.dp))
                 Text(
                     text = "Registro",
                     color = Color.White,
                     fontSize = 24.sp,
-                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.rotate(90f).padding(top = 20.dp)
                 )
+                Spacer(modifier = Modifier.height(100.dp))
             }
         }
     }
 }
-
